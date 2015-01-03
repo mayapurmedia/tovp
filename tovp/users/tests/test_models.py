802c4d46e10d5128f1253f0dd37981932725cfbc
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2015-01-03 08:21:40 +0530

    Added test for user.get_absolute_url() and user.get_link().

--- tovp/users/tests/test_models.py
+++ tovp/users/tests/test_models.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.test import Client
+from django.core.urlresolvers import reverse
 
 from ..models import User
 
@@ -31,3 +33,33 @@ class UserModelTests(TestCase):
         self.assertEqual(user.get_name(), 'Prahlad Nrsimha Das')
         self.assertEqual(user.get_name(), user.get_full_name())
         self.assertEqual(user.get_name(), user.get_short_name())
+
+    def test_get_link(self):
+        from jinja2 import Markup
+        self.assertEqual(self.user.get_link(),
+                         Markup('<a href="/users/user/">Normal User</a>'))
+
+        self.assertEqual(
+            self.user.get_link(css_class='test'),
+            Markup('<a class="test" href="/users/user/">Normal User</a>'))
+
+    def test_get_absolute_url_not_logged(self):
+        client = Client()
+
+        # for not logged in user there should be redirect
+        response = client.get(self.user.get_absolute_url())
+        self.assertEqual(response.status_code, 302)
+
+        # not logged users should be redirected to login page first
+        self.assertRedirects(
+            response,
+            '%s?next=%s' % (reverse('users:login'),
+                            reverse('users:detail', args=['user'])),
+            status_code=302,
+            target_status_code=200)
+
+    def test_get_absolute_url_logged_in(self):
+        client = Client()
+        client.login(username='user', password=self.password)
+        response = client.get(self.user.get_absolute_url())
+        self.assertEqual(response.status_code, 200)
