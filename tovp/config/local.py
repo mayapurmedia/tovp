546c7862e831d753e9eb51e6a4d3709aacd12cd2
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2015-02-01 08:45:53 +0530

    Commented internal IPs.

--- tovp/config/local.py
+++ tovp/config/local.py
@@ -35,7 +35,7 @@ class Local(Common):
     MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
     INSTALLED_APPS += ('debug_toolbar',)
 
-    INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
+    # INTERNAL_IPS = ('127.0.0.1', '10.0.2.2',)
 
     DEBUG_TOOLBAR_CONFIG = {
         'DISABLE_PANELS': [
