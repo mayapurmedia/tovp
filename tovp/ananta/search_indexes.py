443a5a6583fd5741d9c0711a1c17bdf130086cf9
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2015-02-19 16:46:48 +0530

    Added record_id into search index.

--- tovp/ananta/search_indexes.py
+++ tovp/ananta/search_indexes.py
@@ -2,6 +2,7 @@ from haystack import indexes
 
 
 class ContentSearchIndexMixin(indexes.SearchIndex):
+    record_id = indexes.IntegerField(model_attr='pk')
     created = indexes.DateTimeField(model_attr='created')
     modified = indexes.DateTimeField(model_attr='modified')
     absolute_url = indexes.CharField(model_attr='get_absolute_url')
