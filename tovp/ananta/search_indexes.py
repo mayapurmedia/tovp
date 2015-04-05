cf5143337929ca75e20ed521710d1e1fc891f1ff
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2015-04-05 12:34:38 +0530

    Added created_by and modified_by search mixin.

--- tovp/ananta/search_indexes.py
+++ tovp/ananta/search_indexes.py
@@ -7,9 +7,19 @@ class ContentSearchIndexMixin(indexes.SearchIndex):
     modified = indexes.DateTimeField(model_attr='modified')
     absolute_url = indexes.CharField(model_attr='get_absolute_url')
     content_type = indexes.CharField(faceted=True)
+    created_by = indexes.CharField(faceted=True)
+    modified_by = indexes.CharField(faceted=True)
 
     def prepare_content_type(self, obj):
         try:
             return self.content_name
         except:
             return 'Noname'
+
+    def prepare_created_by(self, obj):
+        if obj.created_by:
+            return obj.created_by.display_name
+
+    def prepare_modified_by(self, obj):
+        if obj.modified_by:
+            return obj.modified_by.display_name
