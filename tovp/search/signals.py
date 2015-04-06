a9fd9595ae747bac17f08ae4fa3b649a40d5883e
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2015-04-06 17:30:40 +0530

    Fixed bug when signal for deletion was iterating function instead of its results.

--- tovp/search/signals.py
+++ tovp/search/signals.py
@@ -5,17 +5,13 @@ class RelatedRealtimeSignalProcessor(RealtimeSignalProcessor):
     def handle_save(self, sender, instance, **kwargs):
         if hasattr(instance, 'reindex_related'):
             for related in instance.reindex_related():
-                print(related)
-                # related_objects = getattr(instance, related)
-                # for related_object in related_objects.all():
-                #     print(related_object)
-                #     self.handle_save(related_object.__class__, related_object)
                 self.handle_save(related.__class__, related)
-        return super(RelatedRealtimeSignalProcessor, self).handle_save(sender, instance, **kwargs)
+        return super(RelatedRealtimeSignalProcessor, self). \
+            handle_save(sender, instance, **kwargs)
 
     def handle_delete(self, sender, instance, **kwargs):
         if hasattr(instance, 'reindex_related'):
-            for related in instance.reindex_related:
-                related_obj = getattr(instance, related)
-                self.handle_delete(related_obj.__class__, related_obj)
-        return super(RelatedRealtimeSignalProcessor, self).handle_delete(sender, instance, **kwargs)
+            for related in instance.reindex_related():
+                self.handle_delete(related.__class__, related)
+        return super(RelatedRealtimeSignalProcessor, self). \
+            handle_delete(sender, instance, **kwargs)
