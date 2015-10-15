45b032bd00daf1ee888328575d0605c5639462d0
Petr Vacha <pnd@mayapurmedia.com>
2015-10-15 13:08:08 +0530

    Unless currency defines need of custom function it will use comma formatter and will show only currency of the pledge.

--- tovp/promotions/helpers.py
+++ tovp/promotions/helpers.py
@@ -4,18 +4,21 @@ from ananta.helpers import format_with_commas, format_for_india
 
 
 @register.filter
-def promo_ballance(promotion, ballance):
-    format_functions = {
+def promo_ballance(promotion, pledge, ballance):
+    custom_format_functions = {
         'INR': format_for_india,
-        'USD': format_with_commas,
-        'GBP': format_with_commas,
-        'EUR': format_with_commas,
     }
+
     output = []
-    for currency in ballance:
-        missing_amount = promotion.get_amount(currency) - (
-            ballance[currency]['pledged'] - ballance[currency]['used'])
-        output.append('{amount} {currency}'.format(
-            amount=format_functions[currency](missing_amount),
-            currency=currency))
+    currency = pledge.currency
+    if currency in custom_format_functions:
+        format_function = custom_format_functions[currency]
+    else:
+        format_function = format_with_commas
+
+    missing_amount = promotion.get_amount(currency) - (
+        ballance[currency]['pledged'] - ballance[currency]['used'])
+    output.append('{amount} {currency} more required'.format(
+        amount=format_function(missing_amount),
+        currency=currency))
     return ' / '.join(output)
