60dd98f241e83d9b5a49c4ec123feaf6262d48cd
Petr Vacha <pnd@mayapurmedia.com>
2015-12-11 16:01:18 +0530

    Renamed extension of template file.

--- tovp/contacts/views.py
+++ tovp/contacts/views.py
@@ -42,7 +42,7 @@ class PersonUpdateView(RevisionCommentMixin, LoginRequiredMixin,
                        PermissionRequiredMixin, UpdateView):
     model = Person
     permission_required = "contacts.change_person"
-    template_name = 'contacts/person_form.html'
+    template_name = 'contacts/person_form.jinja'
     form_class = PersonForm
 
     def get_context_data(self, **kwargs):
