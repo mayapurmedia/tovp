a2eaadbc93604e6329726633a57f8a8871981fc8
Petr Vacha / Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2016-04-28 12:52:40 +0530

    Updated migration for default receipt type.

--- tovp/contributions/migrations/0055_auto_20160428_1251.py
+++ tovp/contributions/migrations/0055_auto_20160428_1251.py
@@ -7,13 +7,13 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contributions', '0053_auto_20160414_1136'),
+        ('contributions', '0054_auto_20160419_1750'),
     ]
 
     operations = [
         migrations.AlterField(
             model_name='contribution',
             name='receipt_type',
-            field=models.CharField(choices=[('mayapur-receipt', 'Mayapur Receipt'), ('usa-receipt', 'USA Receipt'), ('external-receipt', 'External / Non Receipt')], max_length=100, verbose_name='Receipt Type', default='external-receipt'),
+            field=models.CharField(verbose_name='Receipt Type', choices=[('mayapur-receipt', 'Mayapur Receipt'), ('usa-receipt', 'USA Receipt'), ('external-receipt', 'External / Non Receipt')], default='external-receipt', max_length=100),
         ),
     ]
