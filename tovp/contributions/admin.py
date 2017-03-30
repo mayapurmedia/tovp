e4de9af9640aada09eba82d8628af0c78bb9bf92
Petr Vacha / Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2017-03-30 15:26:23 +0530

    Added BulkPayment into Django Admin.

--- tovp/contributions/admin.py
+++ tovp/contributions/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from reversion_compare.admin import CompareVersionAdmin
 
-from .models import Pledge, Contribution
+from .models import Pledge, BulkPayment, Contribution
 
 
 class PledgeModelAdmin(CompareVersionAdmin):
@@ -9,10 +9,16 @@ class PledgeModelAdmin(CompareVersionAdmin):
         model = Pledge
 
 
+class BulkPaymentModelAdmin(CompareVersionAdmin):
+    class Meta:
+        model = BulkPayment
+
+
 class ContributionModelAdmin(CompareVersionAdmin):
     class Meta:
         model = Contribution
 
 
+admin.site.register(BulkPayment, BulkPaymentModelAdmin)
 admin.site.register(Pledge, PledgeModelAdmin)
 admin.site.register(Contribution, ContributionModelAdmin)
